fde7624ce2de1b58dad36302ff1e1423f9c31350
adictonator <adityabhaskarsharma@gmail.com>
2022-11-09 16:17:01 +0530

    fixed lottery result value api call

--- src/pages/lotteries/[id]/index.jsx
+++ src/pages/lotteries/[id]/index.jsx
@@ -85,8 +85,8 @@ export default ({ details, results }) => {
 
 export const getStaticProps = async ({ params }) => {
 	const details = await getProductByID(params.id)
-	//const results = await getLotteryResults(details?.lottery?.id)
-	const results = await getLotteryResults(1)
+	const results = await getLotteryResults(details?.lottery?.id)
+	//const results = await getLotteryResults(1)
 
 	return {
 		props: { details, results },
