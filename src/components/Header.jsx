1c91616ca08d4f77518a86f36ab0ea89f23ac627
adictonator <adityabhaskarsharma@gmail.com>
2022-11-13 18:24:44 +0530

    some minor header UI tweaks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,40 @@
-import Image from 'next/image'
+import { UserIcon } from '@heroicons/react/24/solid'
+import { ArrowDownTrayIcon } from '@heroicons/react/24/outline'
+import cs from 'classnames'
 import Link from 'next/link'
+import IconWallet from './Icons/IconWallet'
 import Logo from './Logo'
 import Nav from './Nav'
 
 export default () => {
+	const HeaderLink = ({ href, children, className }) => (
+		<Link href={href}>
+			<a className={cs('flex gap-x-2', className)}>{children}</a>
+		</Link>
+	)
+
 	const TopToolbar = () => (
-		<div className="flex justify-end space-x-5">
+		<div className="flex items-center justify-end space-x-5">
 			<div className="flex flex-col text-xs">
 				<span>Current Time: 4:25 PM</span>
 				<span>Current Session: 00:14:55</span>
 			</div>
-			<button type="button">Add Funds</button>
-			<a href="#">Balance: $45</a>
-			<a href="#">John Doe</a>
+			<button
+				type="button"
+				className="flex items-center gap-x-2 rounded-lg bg-gradient-to-r from-green-600 to-lime-500 px-6 py-2 text-sm text-white hover:from-lime-500 hover:to-lime-500">
+				<ArrowDownTrayIcon className="w-4 text-white" />
+				Add Funds
+			</button>
+			<a href="#" className="flex items-center text-sm text-gray-700">
+				<IconWallet className={'mr-2 w-4 fill-current text-gray-500'} />
+				Balance: <strong>$45</strong>
+			</a>
+			<HeaderLink href={'#'}>
+				<UserIcon className="w-4 text-gray-500" />
+				<span className="flex items-center text-sm text-gray-700">
+					John Doe
+				</span>
+			</HeaderLink>
 		</div>
 	)
 
---
+++ src/components/Icons/IconEnvelopeCircle.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default ({ className }) => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 23 23"
+			className={className}>
+			<g transform="translate(-790.5 -793.5)">
+				<path
+					strokeWidth="1"
+					d="M802 794a11 11 0 1011 11 11 11 0 00-11-11z"></path>
+				<path
+					fill="#fff"
+					d="M571.019 960.629h-9.6a1.224 1.224 0 00-1.194 1.25l-.006 7.5a1.23 1.23 0 001.2 1.25h9.6a1.229 1.229 0 001.2-1.25v-7.5a1.229 1.229 0 00-1.2-1.25zm0 2.5l-4.8 3.125-4.8-3.125v-1.25l4.8 3.125 4.8-3.125z"
+					transform="translate(235.781 -160.629)"></path>
+			</g>
+		</svg>
+	)
+}
